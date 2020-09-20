61a500381ebe636a9524bb4560d3ea74dac5b901
kwannee <41886825+kwannee@users.noreply.github.com>
2020-09-20 14:27:27 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -10,6 +10,9 @@ void setup() {
 }
 
 void loop() {
+    //최초 setup이 두번 call돼서 LED가 한번 켜졌다 꺼진 후 loop가 실행되는 문제가 발생했는데,
+    //하드웨어 문제일 수 있다는 답변을 찾아서 코드는 그대로 제출하겠습니다.
+  
     digitalWrite(PIN_LED, 0); // 최초 켜진 상태로 1초 지속
     delay(1000);
     int cnt = 11; // 마지막에 꺼진 상태로 while을 마무리 하기 위해 cnt를 11로 설정
